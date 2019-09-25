7d08342dc38a556518559541095a350adfde2371
Timothy Sullivan <tsullivan@elastic.co>
2019-09-25 11:11:52 -0700

    remove unusable

--- constants.ts
+++
@@ -1,2 +0,0 @@
-export const MAPS_INGEST_TEMPLATE_NAME = 'elastic-webinar-maps-example';
-export const MAPS_INGEST_TASK_TYPE = 'suricata_ingest';
--- server/lib/suricata/index.ts
+++
@@ -1,1 +0,0 @@
-export { fakeSuricataIngestTask } from './task';
